3794bf1c3f2cdca8dee64982404b8bb40d2f03f1
Pablo de la Pena <p@shedcollective.org>
2014-07-30 00:09:22 +0100

    Bug: sidebar_home list-group needs wrapped in a <div>

--- views/front/_components/sidebar_home.php
+++ views/front/_components/sidebar_home.php
@@ -1,66 +1,68 @@
 <hr class="hidden-md hidden-lg" />
-<ul class="sidebar-home col-md-3 col-md-pull-9 list-group">
-<?php
+<div class="sidebar-home col-md-3 col-md-pull-9">
+	<ul class="list-group">
+	<?php
 
-	echo '<li class="list-group-item">';
-		echo '<h3 class="list-group-item-heading">Categories</h3>';
-		echo '<ul class="list-unstyled rsaquo-list">';
+		echo '<li class="list-group-item">';
+			echo '<h3 class="list-group-item-heading">Categories</h3>';
+			echo '<ul class="list-unstyled rsaquo-list">';
 
-			foreach( $categories AS $category ) :
+				foreach( $categories AS $category ) :
 
-				echo '<li>';
-					echo anchor( $category->url, $category->label );
-				echo '</li>';
+					echo '<li>';
+						echo anchor( $category->url, $category->label );
+					echo '</li>';
 
-			endforeach;
+				endforeach;
 
-		echo '</ul>';
-	echo '</li>';
+			echo '</ul>';
+		echo '</li>';
 
-	echo '<li class="list-group-item">';
-		echo '<h3 class="list-group-item-heading">Brands</h3>';
-		echo '<ul class="list-unstyled rsaquo-list">';
+		echo '<li class="list-group-item">';
+			echo '<h3 class="list-group-item-heading">Brands</h3>';
+			echo '<ul class="list-unstyled rsaquo-list">';
 
-			foreach( $brands AS $brand ) :
+				foreach( $brands AS $brand ) :
 
-				echo '<li>';
-					echo anchor( $brand->url, $brand->label );
-				echo '</li>';
+					echo '<li>';
+						echo anchor( $brand->url, $brand->label );
+					echo '</li>';
 
-			endforeach;
+				endforeach;
 
-		echo '</ul>';
-	echo '</li>';
+			echo '</ul>';
+		echo '</li>';
 
-	echo '<li class="list-group-item">';
-		echo '<h3 class="list-group-item-heading">Ranges</h3>';
-		echo '<ul class="list-unstyled rsaquo-list">';
+		echo '<li class="list-group-item">';
+			echo '<h3 class="list-group-item-heading">Ranges</h3>';
+			echo '<ul class="list-unstyled rsaquo-list">';
 
-			foreach( $ranges AS $range ) :
+				foreach( $ranges AS $range ) :
 
-				echo '<li>';
-					echo anchor( $range->url, $range->label );
-				echo '</li>';
+					echo '<li>';
+						echo anchor( $range->url, $range->label );
+					echo '</li>';
 
-			endforeach;
+				endforeach;
 
-		echo '</ul>';
-	echo '</li>';
+			echo '</ul>';
+		echo '</li>';
 
-	echo '<li class="list-group-item">';
-		echo '<h3 class="list-group-item-heading">Collections</h3>';
-		echo '<ul class="list-unstyled rsaquo-list">';
+		echo '<li class="list-group-item">';
+			echo '<h3 class="list-group-item-heading">Collections</h3>';
+			echo '<ul class="list-unstyled rsaquo-list">';
 
-			foreach( $collections AS $collection ) :
+				foreach( $collections AS $collection ) :
 
-				echo '<li>';
-					echo anchor( $collection->url, $collection->label );
-				echo '</li>';
+					echo '<li>';
+						echo anchor( $collection->url, $collection->label );
+					echo '</li>';
 
-			endforeach;
+				endforeach;
 
-		echo '</ul>';
-	echo '</li>';
+			echo '</ul>';
+		echo '</li>';
 
-?>
-</ul>
+	?>
+	</ul>
+</div>
