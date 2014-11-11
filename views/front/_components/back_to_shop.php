0683fc1a00b6b87ad516dc18da85ada5c10a66f4
Gary Duncan <g@shedcollective.org>
2014-11-11 17:31:39 +0000

    Back Facility on Product Pages
    Back link on single product pages. This should be replaced eventually with a smart breadcrumb that can detect the path the user took to reach a product.

---
+++ views/front/_components/back_to_shop.php
@@ -0,0 +1,10 @@
+<?php
+
+	echo '<ol class="breadcrumb">';
+
+		echo '<li class="crumb home">';
+			echo '<span class="fa fa-angle-left text-muted"></span>';
+			echo '<a onclick="history.go(-1);" style="cursor:pointer;">Go back</a>';
+		echo '</li>';
+
+	echo '</ol>';
--- views/front/product/single.php
+++ views/front/product/single.php
@@ -1,7 +1,8 @@
 <div class="nails-shop-skin-front-classic browse product single">
 	<div class="row">
 	<?php
-
+    	
+		$this->load->view( $skin_front->path . 'views/front/_components/back_to_shop' );
 		$this->load->view( $skin_front->path . 'views/front/_components/product_single' );
 
 	?>
